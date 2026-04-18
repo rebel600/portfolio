b5fa5022c3d2669809d9329fb6e047f385ac6da7
rebel600 <shyampareek232@gmail.com>
2026-04-19 01:22:07 +0530

    added project data in mock utility

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,21 +1,5 @@
-import projectOne from "@/assets/projectOne.png";
-import projectTwo from "@/assets/projectTwo.png";
-import portfolioImage from "@/assets/portfolioWebsite.png";
 import GenericCard from "@/components/GenericCard";
-
-interface ProjectCard {
-  bgColor: string;
-  textColor: string;
-  content: {
-    image?: string;
-    imageAlt?: string;
-    tags?: string[];
-    title: string;
-    description: string;
-    ctaText?: string;
-    ctaHref?: string;
-  };
-}
+import { genericCardContent } from "@/utils/mock";
 
 const Projects = () => {
   return (
@@ -50,46 +34,4 @@ const Projects = () => {
 
 export default Projects;
 
-const genericCardContent: ProjectCard[] = [
-  {
-    bgColor: "bg-white",
-    textColor: "text-slate-600",
-    content: {
-      image: projectOne,
-      imageAlt: "WannaEat - Food Ordering Platform",
-      tags: ["React", "Node.js", "MongoDB"],
-      title: "WannaEat - Food Ordering Platform",
-      description:
-        "A full-stack e-commerce platform built with React, Node.js, and MongoDB.",
-      ctaText: "Explore Project",
-      ctaHref: "https://www.wannaeat.com",
-    },
-  },
-  {
-    bgColor: "bg-white",
-    textColor: "text-slate-600",
-    content: {
-      image: projectTwo,
-      imageAlt: "Escapely - Escape the City",
-      tags: ["React", "ReduxToolKit", "MongoDB"],
-      title: "Escapely - Escape the City",
-      description: `Helped craft a seamless, "no-download" web app experience that bridges the gap between physical puzzle assets and real-world environments. Focused on creating an intuitive hint system and interactive storyline interface that allows users to navigate urban landscapes while solving complex, multi-layered puzzles.`,
-      ctaText: "Explore Project",
-      ctaHref: "https://escapely.com/escape-the-city",
-    },
-  },
-  {
-    bgColor: "bg-white",
-    textColor: "text-slate-600",
-    content: {
-      image: portfolioImage,
-      imageAlt: "Portfolio Website",
-      tags: ["React", "Tailwind CSS", "Typescript"],
-      title: "Portfolio Website",
-      description:
-        "A modern and responsive portfolio website built with React.js, Tailwind CSS and Typescript.",
-      ctaText: "Explore Project",
-      ctaHref: "#",
-    },
-  },
-];
+
