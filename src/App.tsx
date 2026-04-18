b2b0d3d9e55e1cdc3ef4636746a782619b57d31e
rebel600 <shyampareek232@gmail.com>
2026-04-18 22:45:53 +0530

    feat:animate-mouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, lazy } from "react";
 import { useDocumentTitle } from "@/hooks/useDocumentTitle";
 import { useThemeContext } from "./context/theme-provider";
+const AnimateMouse  = lazy(() => import("@/components/AnimateMouse"));
 const Navbar = lazy(() => import("@/components/Navbar"));
 const HeroSection = lazy(() => import("@/pages/hero-section"));
 const About = lazy(() => import("@/pages/about"));
@@ -61,6 +62,7 @@ const App = () => {
   return (
     <div className={`${isDarkMode ? "dark" : ""}`}>
       <div className="min-h-screen dark:bg-slate-950 text-slate-900 dark:text-slate-50 font-sans selection:bg-blue-200 dark:selection:bg-blue-900 selection:text-blue-900 dark:selection:text-blue-100 flex flex-col transition-colors duration-300">
+        <AnimateMouse />
         {/* Navigation */}
         <Navbar
           isMenuOpen={isMenuOpen}
